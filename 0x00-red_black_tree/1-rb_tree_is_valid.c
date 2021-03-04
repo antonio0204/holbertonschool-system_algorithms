af1ff7ef4f86cac0d36224f9d894db5ba1216f65
Samuel Pomeroy <1690@holbertonschool.com>
2021-03-04 03:11:12 +0000

    removed error msg include; black height of NULL leaf now 1

--- 0x00-red_black_tree/1-rb_tree_is_valid.c
+++ 0x00-red_black_tree/1-rb_tree_is_valid.c
@@ -1,8 +1,5 @@
 #include "rb_trees.h"
 
-/* fprintf */
-#include <stdio.h>
-
 /* INT_MIN */
 #include <limits.h>
 
@@ -23,6 +20,7 @@ size_t max_size_t(size_t a, size_t b)
 /**
  * RBT_check_black_height - recursively tests if binary tree has Red-Black Tree
  * property of equal number of black nodes between any leaf and root
+ * note: black height of a NULL leaf is defined to be 1
  *
  * @tree: root node from which to measure, starting at 0
  * Return: levels from root, or 0 if `tree` is NULL
@@ -30,7 +28,7 @@ size_t max_size_t(size_t a, size_t b)
 size_t RBT_check_black_height(const rb_tree_t *tree)
 {
 	if (!tree)
-		return (0);
+		return (1);
 
 	if (tree->color == BLACK)
 		return (1 + max_size_t(RBT_check_black_height(tree->left),
