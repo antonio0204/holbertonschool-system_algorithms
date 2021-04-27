a6dac94b806beee47985e2a8ecbf209177ecdb1d
Samuel Pomeroy <1690@holbertonschool.com>
2021-04-27 00:57:14 +0000

    header and Makefile setup

--- 0x02-huffman_coding/heap/heap.h
+++ 0x02-huffman_coding/heap/heap.h
@@ -1,6 +1,10 @@
 #ifndef _HEAP_H_
 #define _HEAP_H_
 
+/* size_t */
+#include <stddef.h>
+
+
 /**
  * struct binary_tree_node_s - Binary tree node data structure
  *
@@ -17,6 +21,7 @@ typedef struct binary_tree_node_s
 	struct binary_tree_node_s *parent;
 } binary_tree_node_t;
 
+
 /**
  * struct heap_s - Heap data structure
  *
@@ -30,5 +35,29 @@ typedef struct heap_s
 	int (*data_cmp)(void *, void *);
 	binary_tree_node_t *root;
 } heap_t;
+
+
+/* task 0. Min Binary Heap - Create heap */
+/* task 2. Min Binary Heap - Insert node */
+/* heap_create.c */
+heap_t *heap_create(int (*data_cmp)(void *, void *));
+
+/* task 1. Min Binary Heap - Create node */
+/* task 2. Min Binary Heap - Insert node */
+/* binary_tree_node.c */
+binary_tree_node_t *binary_tree_node(binary_tree_node_t *parent, void *data);
+
+/* task 2. Min Binary Heap - Insert node */
+/* heap_insert.c */
+binary_tree_node_t *heap_insert(heap_t *heap, void *data);
+
+/* task 3. Min Binary Heap - Extract */
+/* heap_extract.c */
+void *heap_extract(heap_t *heap);
+
+/* task 4. Min Binary Heap - Delete heap */
+/* heap_delete.c */
+void heap_delete(heap_t *heap, void (*free_data)(void *));
+
 
 #endif /* _HEAP_H_ */
--- 0x02-huffman_coding/huffman.h
+++ 0x02-huffman_coding/huffman.h
@@ -1,6 +1,10 @@
 #ifndef HUFFMAN_H
 #define HUFFMAN_H
 
+/* size_t */
+#include <stddef.h>
+
+
 /**
  * struct symbol_s - Stores a char and its associated frequency
  *
@@ -12,5 +16,27 @@ typedef struct symbol_s
 	char data;
 	size_t freq;
 } symbol_t;
+
+
+/* task 5. Symbols: Create symbol */
+/* symbol.c */
+symbol_t *symbol_create(char data, size_t freq);
+
+/* task 6. Huffman coding - Step 1: Priority queue */
+/* huffman_priority_queue.c */
+heap_t *huffman_priority_queue(char *data, size_t *freq, size_t size);
+
+/* task 7. Huffman coding - Step 2: Extract */
+/* huffman_extract_and_insert.c */
+int huffman_extract_and_insert(heap_t *priority_queue);
+
+/* task 8. Huffman coding - Step 3: Build Huffman tree */
+/* huffman_tree.c */
+binary_tree_node_t *huffman_tree(char *data, size_t *freq, size_t size);
+
+/* task 9. Huffman coding - Step 4: Huffman codes */
+/* huffman_codes.c */
+int huffman_codes(char *data, size_t *freq, size_t size);
+
 
 #endif /* HUFFMAN_H */
