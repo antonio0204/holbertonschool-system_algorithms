a136d147e52b38938f504aff0d7c54a13479e765
Samuel Pomeroy <1690@holbertonschool.com>
2021-04-27 01:33:19 +0000

    first attempts t0,1

---
+++ 0x02-huffman_coding/heap/binary_tree_node.c
@@ -0,0 +1,30 @@
+#include "heap.h"
+/* malloc */
+#include <stdlib.h>
+
+
+/**
+ * binary_tree_node - creates a generic Binary Tree node
+ *
+ * @parent: pointer to node assigned as parent of created node
+ * @data: data to be stored in the node
+ * Return: pointer to the created node, or NULL on failure
+ */
+binary_tree_node_t *binary_tree_node(binary_tree_node_t *parent, void *data)
+{
+	binary_tree_node_t *new;
+
+	if (!data)
+		return (NULL);
+
+	new = malloc(sizeof(binary_tree_node_t));
+	if (!new)
+		return (NULL);
+
+	new->data = data;
+	new->left = NULL;
+	new->right = NULL;
+	new->parent = parent;
+
+	return (new);
+}
---
+++ 0x02-huffman_coding/heap/heap_create.c
@@ -0,0 +1,27 @@
+#include "heap.h"
+/* malloc */
+#include <stdlib.h>
+
+/**
+ * heap_create - allocates a new heap_t heap data structure profile
+ *
+ * @data_cmp: pointer to a comparison function
+ * Return: pointer to the created heap_t structure, or NULL on failure
+ */
+heap_t *heap_create(int (*data_cmp)(void *, void *))
+{
+	heap_t *new;
+
+	if (!data_cmp)
+		return (NULL);
+
+	new = malloc(sizeof(heap_t));
+	if (!new)
+		return (NULL);
+
+	new->size = 0;
+	new->data_cmp = data_cmp;
+	new->root = NULL;
+
+	return (new);
+}
