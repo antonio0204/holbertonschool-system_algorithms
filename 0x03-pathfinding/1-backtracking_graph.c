f30af1a4f0674679e68af5569dbac2c818b567cd
Samuel Pomeroy <1690@holbertonschool.com>
2021-05-22 22:25:17 +0000

    first draft t1

--- 0x03-pathfinding/1-backtracking_graph.c
+++ 0x03-pathfinding/1-backtracking_graph.c
@@ -1,4 +1,162 @@
 #include "pathfinding.h"
+/* malloc free */
+#include <stdlib.h>
+/* printf */
+#include <stdio.h>
+/* strcmp strdup */
+#include <string.h>
+
+#include <unistd.h>
+
+
+/**
+  * backtrackPath - deletes record of path traveled from dead end back to last
+  *   junction point
+  *
+  * @path: queue of visited graph node content, represents current backtracking
+  *   solution candidate
+  * @last_fork: vertex at last junction point
+  */
+void backtrackPath(queue_t *path, const vertex_t *last_fork)
+{
+	queue_node_t *node = NULL;
+        char *vertex_content = NULL;
+
+	if (!path || !path->front || !path->back || !last_fork)
+		return;
+
+	node = path->back;
+	if (node)
+		vertex_content = (char *)node->ptr;
+
+	while (node && vertex_content &&
+	       strcmp(vertex_content, last_fork->content) != 0)
+	{
+		/* dequeue from back */
+		if (node && node->prev)
+			node->prev->next = NULL;
+		path->back = node->prev;
+		if (path->back == NULL)
+			path->front = NULL;
+
+		free(node);
+		free(vertex_content);
+
+		node = path->back;
+		if (node)
+		        vertex_content = (char *)node->ptr;
+	}
+}
+
+
+/**
+  * isVisitedVertex - checks a queue with `void *` data points, assuming
+  *   they are all castable to `vertex_t *`, to find a vertex with content
+  *   matching `vertex`
+  *
+  * @path: queue to search
+  * @vertex: vertex containing data to search for
+  * Return: first queue node containing matching data, or NULL on failure
+  */
+queue_node_t *isVisitedVertex(queue_t *path, const vertex_t *vertex)
+{
+	queue_node_t *temp = NULL;
+        char *vertex_content = NULL;
+
+	if (!path || !path->front || !path->back || !vertex)
+		return (NULL);
+
+	temp = path->front;
+	while (temp)
+	{
+		if (temp->ptr)
+		{
+		        vertex_content = (char *)temp->ptr;
+
+			if (strcmp(vertex->content, vertex_content) == 0)
+				return (temp);
+		}
+
+		temp = temp->next;
+	}
+
+	return (NULL);
+}
+
+
+/**
+  * graphDFS - helper to backtracking_graph, searches for the
+  *   first path from a starting point to a target point within a
+  *   graph using a recursive depth-first search
+  *
+  * @path: queue of nodes visited, represents current candidate solution
+  * @graph: pointer to graph to traverse
+  * @curr: current vertex in graph
+  * @target: target vertex in graph
+  * Return: 1 if target found at or below current recursion frame,
+  *   0 if not or failure
+  */
+int graphDFS(queue_t *path, graph_t *graph,
+	     const vertex_t *curr, const vertex_t *target)
+{
+        char *content_copy = NULL;
+	int target_found = 0;
+	edge_t *temp_e = NULL;
+
+	queue_node_t *temp = NULL;
+        char *vertex_content = NULL;
+
+	if (!path || !graph || !curr || !target)
+		return (0);
+
+	printf("Checking %s\n", curr->content);
+
+	content_copy = strdup(curr->content);
+	if (!content_copy)
+	{
+		perror("graphDFS: strdup");
+		return (0);
+	}
+
+	if (!queue_push_back(path, (void *)content_copy))
+	{
+		printf("\tqueue_push_back fail for %s\n", content_copy);
+		free(content_copy);
+		return (0);
+	}
+
+	printf("\tvisited updates to: ");
+	temp = path->front;
+	while (temp)
+	{
+		if (temp->ptr)
+		{
+		        vertex_content = (char *)temp->ptr;
+			printf("%s ", vertex_content);
+		}
+
+		temp = temp->next;
+	}
+	putchar('\n');
+
+	if (strcmp(curr->content, target->content) == 0)
+		return (1);
+
+	for (temp_e = curr->edges; !target_found && temp_e && temp_e->next;
+	     temp_e = temp_e->next)
+	{
+		if (!isVisitedVertex(path, temp_e->dest))
+		{
+			target_found |= graphDFS(path, graph,
+						temp_e->dest, target);
+
+			if (!target_found)
+				backtrackPath(path, curr);
+		}
+	}
+
+	return (target_found);
+}
 
 
 /**
@@ -14,6 +172,21 @@
 queue_t *backtracking_graph(graph_t *graph, vertex_t const *start,
 			    vertex_t const *target)
 {
+	queue_t *path = NULL;
+
 	if (!graph || !start || !target)
 		return (NULL);
+
+	path = queue_create();
+	if (!path)
+		return (NULL);
+
+	if (!graphDFS(path, graph, start, target))
+	{
+		/* assumes that free() can be used with node data */
+		queue_delete(path);
+		return (NULL);
+	}
+
+	return (path);
 }
