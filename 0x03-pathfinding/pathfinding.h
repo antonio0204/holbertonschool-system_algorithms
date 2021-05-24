59b338595f015c67d66b7d09e8c4db44842760a7
Samuel Pomeroy <1690@holbertonschool.com>
2021-05-24 19:07:36 +0000

    first attempt t100 with auxillary file

--- 0x03-pathfinding/pathfinding.h
+++ 0x03-pathfinding/pathfinding.h
@@ -38,6 +38,27 @@ typedef struct dijkstra_vertex_s
 } dijkstra_vertex_t;
 
 
+/* 100-a_star_graph.c */
+/**
+  * struct a_star_vertex_s - used to implement a Dijkstra priority queue as
+  *   a sortable array
+  *
+  * @vertex: vertex in graph to be traversed
+  * @cml_weight: current cumulative weight of vertex, sum of weights of all
+  *   edges along currently defined optimal path from start
+  * @heuristic: added to cml_weight to set order of priority queue; in this
+  *   case represents the Euclidean distance from vertex to target
+  * @path_via: previous vertex in currently defined optimal path from start
+  */
+typedef struct a_star_vertex_s
+{
+	vertex_t *vertex;
+	size_t cml_weight;
+	size_t heuristic;
+	vertex_t *path_via;
+} a_star_vertex_t;
+
+
 /* 0-backtracking_array.c */
 queue_t *backtracking_array(char **map, int rows, int cols,
 			    point_t const *start, point_t const *target);
@@ -49,6 +70,9 @@ queue_t *backtracking_graph(graph_t *graph, vertex_t const *start,
 /* 2-dijkstra_graph.c */
 queue_t *dijkstra_graph(graph_t *graph, vertex_t const *start,
 			vertex_t const *target);
+
+/* euclideanDist.c */
+size_t euclideanDist(const vertex_t *vertex, const vertex_t *target);
 
 /* 100-a_star_graph.c */
 queue_t *a_star_graph(graph_t *graph, vertex_t const *start,
