c8977808a84bc45f2736ca586ed78060b4972920
Samuel Pomeroy <1690@holbertonschool.com>
2021-07-06 04:50:42 +0000

    0x04 t0 first attempt

--- 0x04-nary_trees/0-nary_tree_insert.c
+++ 0x04-nary_trees/0-nary_tree_insert.c
@@ -1,6 +1,12 @@
 #include "nary_trees.h"
 
-/*If parent is not NULL, the created node must be added at the beginning of the parent’s children linked list.*/
+/* fprintf */
+#include <stdio.h>
+/* strdup */
+#include <string.h>
+/* malloc free */
+#include <stdlib.h>
+
 
 /**
  * nary_tree_insert - inserts a node in a N-ary tree
@@ -12,4 +18,36 @@
  */
 nary_tree_t *nary_tree_insert(nary_tree_t *parent, char const *str)
 {
+	nary_tree_t *new;
+	char *str_copy;
+
+	new = malloc(sizeof(nary_tree_t));
+	if (!new)
+	{
+		fprintf(stderr, "nary_tree_insert: malloc failure\n");
+		return (NULL);
+	}
+
+	str_copy = strdup(str);
+	if (!str_copy)
+	{
+		free(new);
+		fprintf(stderr, "nary_tree_insert: strdup failure\n");
+		return (NULL);
+	}
+
+	new->content = str_copy;
+	new->parent = parent;
+	new->nb_children = 0;
+	new->children = NULL;
+	new->next = NULL;
+
+	if (parent)
+	{
+		new->next = parent->children;
+		parent->children = new;
+		parent->nb_children++;
+	}
+
+	return (new);
 }
