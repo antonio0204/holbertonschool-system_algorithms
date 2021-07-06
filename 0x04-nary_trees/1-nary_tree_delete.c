091a1474e868a7f14673dae3a747fe10e398433d
Samuel Pomeroy <1690@holbertonschool.com>
2021-07-06 05:14:58 +0000

    0x04 t1 first attempt

--- 0x04-nary_trees/1-nary_tree_delete.c
+++ 0x04-nary_trees/1-nary_tree_delete.c
@@ -1,5 +1,8 @@
 #include "nary_trees.h"
 
+/* free */
+#include <stdlib.h>
+
 
 /**
  * nary_tree_delete - deallocates an entire N-ary tree
@@ -8,4 +11,20 @@
  */
 void nary_tree_delete(nary_tree_t *tree)
 {
+	nary_tree_t *temp;
+
+	if (!tree)
+		return;
+
+	while (tree->children)
+	{
+		temp = tree->children;
+		tree->children = tree->children->next;
+		nary_tree_delete(temp);
+	}
+
+	if (tree->content)
+		free(tree->content);
+
+	free(tree);
 }
